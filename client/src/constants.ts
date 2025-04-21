a29440d758a12ab29dffe12d07105df016d3daf7
Ahmadokn <Ahmad.tsw@gmail.com>
2025-04-22 03:35:44 +0400

    Wrong api path

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,6 @@
 // Use localhost during development, otherwise call relative API path
-export const api =
-  process.env.NODE_ENV === "development"
-    ? process.env.REACT_APP_DEV_API_URL || "http://localhost:4000/api"
-    : `${window.location.origin}/api`;
+export const api = `${window.location.origin}/api`;
+// export const api = `${window.location.origin}/api`;
+  // process.env.NODE_ENV === "development"
+  //   ? process.env.REACT_APP_DEV_API_URL || "http://localhost:4000/api"
+  //   : `${window.location.origin}/api`;
