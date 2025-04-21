e13e981f5bfcdfa389e66059d3328b5ff327e36d
Ahmadokn <Ahmad.tsw@gmail.com>
2025-04-22 02:57:49 +0400

    Updated api path

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,5 @@
-export const api = "http://localhost:4000/api";
-  // process.env.NODE_ENV === "development"
-  //   ? process.env.REACT_APP_DEV_API_URL
-  //   : process.env.REACT_APP_PROD_API_URL;
+// Use localhost during development, otherwise call relative API path
+export const api =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_DEV_API_URL || "http://localhost:4000/api"
+    : `${window.location.origin}/api`;
